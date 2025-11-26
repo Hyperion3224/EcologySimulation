7958502fd80cdcfa09f669c9e2a37a17d1b9b4b1
Jaxon <shrimpman911@gmail.com>
2025-11-26 09:44:53 -0600

    added react as webview

--- src/map/board.ts
+++ src/map/board.ts
@@ -1,7 +1,7 @@
-import { size, heightmap } from "../interfaces";
+import { size } from "../interfaces";
 
 export class board {
-    public heightmap = {
+    public heightmap: any = {
         coords: [[],[]],
         height_function: ((x: number,y: number)=>{return 0}),
         size: {x:0, y:0}
@@ -10,10 +10,10 @@ export class board {
     constructor(size: size, height_function: (x: number,y: number) => number){
         this.heightmap.size = size;
         this.heightmap.height_function = height_function;
-        this.load_heightmap();
+        this.load_heightmap(this.heightmap);
     }
 
-    load_heightmap(){
+    load_heightmap(heightmap: any){
         for (let x = 0; x > this.heightmap.size.x; x++){
             for(let y = 0; y > this.heightmap.size.y; y++){
                 heightmap.coords[x][y] = this.heightmap.height_function(x, y);
